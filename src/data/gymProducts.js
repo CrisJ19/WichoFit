7ffc5b6a5b4c0e1bfb28596b6cdf74dd40ed4286
Angel Sanchez <ajsanchezo@ufpso.edu.co>
2025-11-14 03:23:51 -0500

    Add gymProducts.js to version control

---
+++ src/data/gymProducts.js
@@ -0,0 +1,152 @@
+export const gymProducts = [
+  {
+    title: 'Proteína Whey WichoFit 2LB',
+    description: 'Suplemento premium para aumentar masa muscular.',
+    image: 'https://m.media-amazon.com/images/I/61M9X6iE3yL._AC_SL1500_.jpg',
+  },
+  {
+    title: 'Creatina Monohidratada WichoFit',
+    description: 'Creatina 100% pura para fuerza y rendimiento.',
+    image: 'https://m.media-amazon.com/images/I/61H7Q2GC9hL._AC_SL1500_.jpg',
+  },
+  {
+    title: 'Guantes de Gimnasio WichoFit',
+    description: 'Antideslizantes y acolchados para mayor comodidad.',
+    image: 'https://m.media-amazon.com/images/I/71v7lidh8qL._AC_SL1500_.jpg',
+  },
+  {
+    title: 'Pre-Entreno WichoFit NitroX',
+    description: 'Explosión de energía y bombeo muscular.',
+    image: 'https://m.media-amazon.com/images/I/61ZwYg9gnvL._AC_SL1500_.jpg',
+  },
+  {
+    title: 'Banda Elástica Pro WichoFit',
+    description: 'Ideal para calentamiento y glúteos.',
+    image: 'https://m.media-amazon.com/images/I/61su3m9QwVL._AC_SL1500_.jpg',
+  },
+  {
+    title: 'Rodillera de Compresión WichoFit',
+    description: 'Soporte para entrenamientos intensos.',
+    image: 'https://m.media-amazon.com/images/I/71r1zrMXx0L._AC_SL1500_.jpg',
+  },
+  {
+    title: 'Termo Metálico 1L WichoFit',
+    description: 'Mantiene la bebida fría por 24 horas.',
+    image: 'https://m.media-amazon.com/images/I/61fLPNVZ4dL._AC_SL1500_.jpg',
+  },
+  {
+    title: 'Cinturón Lumbar WichoFit',
+    description: 'Protección ideal para peso muerto y sentadilla.',
+    image: 'https://m.media-amazon.com/images/I/71wtabc9TBL._AC_SL1500_.jpg',
+  },
+  {
+    title: 'Toalla Microfibra Gym WichoFit',
+    description: 'Antiolor y ultra absorbente.',
+    image: 'https://m.media-amazon.com/images/I/71eP8Wy8hSL._AC_SL1500_.jpg',
+  },
+  {
+    title: 'Shaker WichoFit 700ml',
+    description: 'Perfecto para proteínas y pre entreno.',
+    image: 'https://m.media-amazon.com/images/I/61Vv4vS1bHL._AC_SL1500_.jpg',
+  },
+  {
+    title: 'Proteína WichoFit Mass Gainer 6LB',
+    description: 'Aumenta masa muscular rápidamente con fórmula alta en calorías.',
+    image: 'https://m.media-amazon.com/images/I/61AT2FZHtAL._AC_SL1500_.jpg',
+  },
+  {
+    title: 'Aminoácidos BCAA WichoFit Recovery',
+    description: 'Mejora la recuperación y evita el catabolismo muscular.',
+    image: 'https://m.media-amazon.com/images/I/61O5eKxn6FL._AC_SL1500_.jpg',
+  },
+  {
+    title: 'Barra de Dominadas WichoFit Pro',
+    description: 'Barra ajustable para puerta, soporte de 200kg.',
+    image: 'https://m.media-amazon.com/images/I/81d1CZ4i6aL._AC_SL1500_.jpg',
+  },
+  {
+    title: 'Esterilla Yoga WichoFit Antideslizante',
+    description: 'Material premium antibacteriano y extra acolchado.',
+    image: 'https://m.media-amazon.com/images/I/71xts97dCiL._AC_SL1500_.jpg',
+  },
+  {
+    title: 'Rueda Abdominal WichoFit CoreX',
+    description: 'La herramienta definitiva para construir abdomen rápido.',
+    image: 'https://m.media-amazon.com/images/I/71jJkmoVa0L._AC_SL1500_.jpg',
+  },
+  {
+    title: 'Cuerda de Saltar WichoFit Speed Rope',
+    description: 'Ultraligera, ideal para cardio intenso y crossfit.',
+    image: 'https://m.media-amazon.com/images/I/71vCp6mBWkL._AC_SL1500_.jpg',
+  },
+  {
+    title: 'Set Mancuernas Ajustables WichoFit 24kg',
+    description: 'Sistema rápido de ajuste para entrenamientos completos.',
+    image: 'https://m.media-amazon.com/images/I/81Wkgi98MHL._AC_SL1500_.jpg',
+  },
+  {
+    title: 'Botella Shakex WichoFit Pro 1.2L',
+    description: 'Tapa segura y mezclador metálico incluido.',
+    image: 'https://m.media-amazon.com/images/I/71y4YBvHhAL._AC_SL1500_.jpg',
+  },
+  {
+    title: 'Faja Deportiva WichoFit Slim',
+    description: 'Perfecta para reventar abdomen y sudar más.',
+    image: 'https://m.media-amazon.com/images/I/81EkF2WWHfL._AC_SL1500_.jpg',
+  },
+  {
+    title: 'Gorra WichoFit Training',
+    description: 'Ligera y transpirable, diseño oficial WichoFit.',
+    image: 'https://m.media-amazon.com/images/I/71ImQxGcBQL._AC_SL1500_.jpg',
+  },
+  {
+    title: 'Protector Bucal WichoFit Combat',
+    description: 'Ideal para boxeo, MMA y artes marciales mixtas.',
+    image: 'https://m.media-amazon.com/images/I/61OxRj3juLL._AC_SL1500_.jpg',
+  },
+  {
+    title: 'Kit de Agarre WichoFit Grip Master',
+    description: 'Fortalece manos, antebrazos y mejora el agarre.',
+    image: 'https://m.media-amazon.com/images/I/61oFwnsPnKL._AC_SL1500_.jpg',
+  },
+  {
+    title: 'Discos Olímpicos WichoFit 10kg (Par)',
+    description: 'Recubiertos en goma premium, silenciosos y duraderos.',
+    image: 'https://m.media-amazon.com/images/I/71Pf63HgVFL._AC_SL1500_.jpg',
+  },
+  {
+    title: 'Barra Olímpica WichoFit 20kg',
+    description: 'Acero endurecido, rodamientos premium.',
+    image: 'https://m.media-amazon.com/images/I/61S2UB3pjgL._AC_SL1500_.jpg',
+  },
+  {
+    title: 'Set Bandas de Resistencia WichoFit 5 Niveles',
+    description: 'Desde suave a muy fuerte, incluye bolsa y guía.',
+    image: 'https://m.media-amazon.com/images/I/71LV21CYGgL._AC_SL1500_.jpg',
+  },
+  {
+    title: 'Masajeador de Percusión WichoFit ProGun',
+    description: '13 cabezales, 30 niveles, ideal para recuperación.',
+    image: 'https://m.media-amazon.com/images/I/71N9z35KoXL._AC_SL1500_.jpg',
+  },
+  {
+    title: 'Chaleco con Peso WichoFit 10kg',
+    description: 'Distribución uniforme del peso y materiales premium.',
+    image: 'https://m.media-amazon.com/images/I/81qVmV7lG5L._AC_SL1500_.jpg',
+  },
+  {
+    title: 'Banco de Pesas WichoFit Inclinado',
+    description: 'Ajustable, soporte 300kg, perfecto para pecho.',
+    image: 'https://m.media-amazon.com/images/I/712Oht9WSqL._AC_SL1500_.jpg',
+  },
+  {
+    title: 'Tensor Elástico WichoFit Pull',
+    description: 'Excelente para espalda, hombros y movilidad.',
+    image: 'https://m.media-amazon.com/images/I/81FoXEQPpHL._AC_SL1500_.jpg',
+  },
+  {
+    title: 'Kit CrossFit WichoFit Starter',
+    description: 'Incluye cuerda, grips, bandas y bolsa.',
+    image: 'https://m.media-amazon.com/images/I/71bMyCV2dZL._AC_SL1500_.jpg',
+  },
+]
